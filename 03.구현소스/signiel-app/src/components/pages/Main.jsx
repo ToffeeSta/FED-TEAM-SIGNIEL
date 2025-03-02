c3e6329e6c526ff7b7650afea11c0d628b964251
ToffeeSta <Wjdgnsrh@gmail.com>
2025-03-02 18:30:48 +0900

    ㅇㅇ

--- 03.구현소스/signiel-app/src/components/pages/Main.jsx
+++ 03.구현소스/signiel-app/src/components/pages/Main.jsx
@@ -1,1 +1,13 @@
 /// 메인페이지 컴포넌트 : Main.jsx /////
+
+import React from 'react';
+
+const Main = () => {
+    return (
+        <div>
+            <h1>메인 페이지 입니다!</h1>
+        </div>
+    );
+};
+
+export default Main;
