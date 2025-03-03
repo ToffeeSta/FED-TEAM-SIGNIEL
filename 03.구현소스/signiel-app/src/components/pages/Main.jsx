ecb0adf89215b9f5e56d9677a2c3ea5e870048d4
ToffeeSta <Wjdgnsrh@gmail.com>
2025-03-04 03:18:29 +0900

    dd

--- 03.구현소스/signiel-app/src/components/pages/Main.jsx
+++ 03.구현소스/signiel-app/src/components/pages/Main.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 const Main = () => {
     return (
         <div>
-            <h1>메인 페이지 입니다!</h1>
+            <h1>메인 페이지</h1>
         </div>
     );
 };
---
+++ 03.구현소스/signiel-app/src/js/data/gnb.js
@@ -0,0 +1,42 @@
+// 롯데호텔 GNB메뉴 데이터
+export const menu = [
+    {
+        txt:"지점소개",
+        link:"/branch",
+        sub:[
+            {
+                txt: "지점1",
+                link: "/branch",
+            },
+            {
+                txt: "지점2",
+                link: "/branch",
+            },
+            {
+                txt: "지점3",
+                link: "/branch",
+            },
+           
+        ],
+    },
+    {
+        txt:"예약",
+        link:"/reservation",
+    },
+    {
+        txt:"게시판",
+        link:"/post",
+    },
+    {
+        txt:"회원정보",
+        link:"/user",
+    },
+    {
+        txt:"로그인",
+        link:"/login",
+    },
+    {
+        txt:"회원가입",
+        link:"/signup",
+    },
+]
