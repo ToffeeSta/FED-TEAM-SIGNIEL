7c709919d9c9c511bec3878b6592ccf3bcd66e6d
Kangsoo01 <angelakang54@gmail.com>
2025-03-10 17:22:07 +0900

    ss

--- 03.구현소스/signiel-app/src/components/pages/Main.jsx
+++ 03.구현소스/signiel-app/src/components/pages/Main.jsx
@@ -2,12 +2,15 @@
 
 import React from 'react';
 
+import "../../css/pages/main.scss";
+import Banner from '../modules/Banner';
 
-function Main() {
+const Main = () => {
     return (
-        <div>
-           <h1>Main</h1>
-        </div>
+        <>
+            {/* 1. 메인배너 */}
+            <Banner />
+        </>
     );
 };
 
