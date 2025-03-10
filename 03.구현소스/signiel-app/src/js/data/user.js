7f5f87a0dc0133f25e42b961c5bf0aa96ea16be7
gogoeu <ko1331@naver.com>
2025-03-10 09:36:10 +0900

    ㅇ
    ㅇ

--- 03.구현소스/signiel-app/src/js/data/user.js
+++ 03.구현소스/signiel-app/src/js/data/user.js
@@ -1,10 +1,4 @@
-// const initDatauser = () => {
-//   // 만약 로컬스토리지 "users"가 없으면 users 데이터를 저장
-//   if (localStorage.getItem("users") === null) {
-//     localStorage.setItem("users", JSON.stringify(users));
-//     // console.log("로컬스토리지 초기 데이터 저장 확인");
-//   }
-// };
+
 
 
 export const users = [
@@ -53,5 +47,3 @@ export const users = [
   ];
 
 
-
-  // export { initDatauser };
--- 03.구현소스/signiel-app/src/js/func/mem_fn.js
+++ 03.구현소스/signiel-app/src/js/func/mem_fn.js
@@ -8,6 +8,18 @@
     5. 이메일 : eml
 ************************************/
 
+
+// const initData = () => {
+//   // 만약 로컬스토리지 "users"가 없으면 users 데이터를 저장
+//   if (localStorage.getItem("users") === null) {
+//     localStorage.setItem("users", JSON.stringify(users));
+//     // console.log("로컬스토리지 초기 데이터 저장 확인");
+//   }
+// };
+
+
+
+
 // [ 로컬쓰 클리어 ] /////////
 const clearData = () => {
   localStorage.clear();
