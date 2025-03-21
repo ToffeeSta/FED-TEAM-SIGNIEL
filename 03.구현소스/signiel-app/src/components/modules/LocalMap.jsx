bfcdb4f899ca4bb7a7c973a4ff4905c7bc7939cb
ToffeeSta <Wjdgnsrh@gmail.com>
2025-03-21 17:03:01 +0900

    고은 머징

---
+++ 03.구현소스/signiel-app/src/components/modules/LocalMap.jsx
@@ -0,0 +1,35 @@
+// 구글맵 컴포넌트 - Google Map 
+// ★반드시 npm i @react-google-maps/api 로 설치해야함!
+
+import React, { memo } from 'react'
+import { GoogleMap, LoadScript, MarkerF } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '500px',
+  // width: '100%',
+  height: '400px',
+  // padding: '0 20px'
+};
+
+// const center = {
+//   lat: 14.018000,
+//   lng: 120.835941
+// };
+
+function LocalMap({center}) {
+    return (
+        <LoadScript
+          googleMapsApiKey="AIzaSyD-n65kmRFWXHWxvLRIy2nHHcKBoSm09Mg"
+        >
+          <GoogleMap
+            mapContainerStyle={containerStyle}
+            center={center}
+            zoom={14}
+          >
+            <MarkerF position={center} icon={"./images/logo/marker.png"}></MarkerF>
+          </GoogleMap>
+        </LoadScript>
+      )
+}
+
+export default memo(LocalMap);
--- 03.구현소스/signiel-app/src/components/pages/Branch.jsx
+++ 03.구현소스/signiel-app/src/components/pages/Branch.jsx
@@ -1,10 +1,16 @@
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect, useRef } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
 import { branchData } from "../../js/data/branch";
 
 // 모듈 CSS 불러오기 /////
-import "../../css/pages/signiel.scss";
+import "../../css/pages/branch.scss";
+import SwiperApp from "../plugin/SwiperApp";
+import LocalMap from "../modules/LocalMap";
+
+/* API 키값 AIzaSyCTZWygfYuMNSJHOB-p16G3P8OxI3SRUcU */
+/* 부산 35.16011, 129.1699 */
+/* 제주 33.49061, 126.4864 */
 
 function Branch(props) {
   const { state } = useLocation();
@@ -22,215 +28,269 @@ function Branch(props) {
   // -> goPage(라우터주소,전달객체)
   // -> 예) goPage('/branch',{state:{local:'seoul'}})
 
+  // 지역별 위도경도값
+  const latLng = useRef({
+    seoul: { lat: 37.51283, lng: 127.1026 },
+    busan: { lat: 35.16011, lng: 129.1699 },
+    jeju: { lat: 33.49061, lng: 126.4864 },
+  });
+
+  // console.log(local,'지역 위도:',latLng.current[local].lat);
+  // console.log(local,'지역 경도:',latLng.current[local].lng);
+
   // 리턴 코드구역 ///////////////
   return (
     <>
-      <div className="main_01">
-        <div>
-          <img
-            className="maing_img"
-            src={process.env.PUBLIC_URL +"/images/branch/" + local + "/main.jpg"}
-            alt="메인"
-          />
-
-          <p>
-            <strong>LOTTE</strong> HOTELS & RESORTS
-          </p>
-          <span className="care">
-            <h1>HOTEL LOTTE</h1>
-            <h2>
-              호텔롯데는 최상의 Care를
-              <br />
-              제공하여 고객의 가치 있는 여정에 함께합니다.
-            </h2>
-          </span>
-        </div>
-      </div>
-      <div className="main_02 main_0">
-        <div>
-          <img src={process.env.PUBLIC_URL +"/images/branch/" + local + "/main_02.png"} />
-        </div>
-        <div>
-          <div className="txt-inbox">
-            <h1>
-              {selData.tit.split("^")[0]}
-              <br />
-              {selData.tit.split("^")[1]}
-            </h1>
-            <p>{selData.cont.split("^")[0]}</p>
-            <p>{selData.cont.split("^")[1]}</p>
-            <p>{selData.cont.split("^")[2]}</p>
-          </div>
-        </div>
-      </div>
-      <div className="main_03 main_0">
-        <img
-          src={process.env.PUBLIC_URL +"/images/branch/" + local + "/main_03.png"}
-          alt="의자"
-        />
-
-        <div>
-          <div>
-          <h1>{selData.tit02}</h1>
-          <p>{selData.cont02.split("^")[0]}</p>
-          <p>{selData.cont02.split("^")[1]}</p>
-          </div>
-        </div>
-      </div>
-      <div className="main_04 main_0">
-        <span>
-          <div className="txt-inbox">
-            <h1>{selData.tit03}</h1>
-          <p>{selData.cont03.split("^")[0]}</p>
-          <p>{selData.cont03.split("^")[1]}</p>
-          </div>
-        </span>
-        <img
-          src={process.env.PUBLIC_URL +"/images/branch/" + local + "/main_04.png"}
-          alt="메인4"
-        />
-      </div>
-
-      {
-        // 서울만 나오는 파트
-        local === "seoul" && (
-          <div className="main_05 main_0">
-            <h1>Room Packages & Offers</h1>
-            <div className="main_05_inbox">
-              <ul>
-                <li>
-                  <img
-                    src={process.env.PUBLIC_URL +"/images/branch/seoul/main_05_01.png"}
-                    alt="메인5_1"
-                  />
+      <div className="branch-container">
+        <div className="branch-cont">
+          <SwiperApp local={local} />
+          {/* max-width 적용 */}
+          <div className="branch-wrap">
+            {/* 1 */}
+            <div className="con-wrap">
+              <div className="img-box">
+                <img
+                  // src={"../../../images/branch/" + local + "/main_01.png"}
+                  src={
+                    process.env.PUBLIC_URL +
+                    "/images/branch/" +
+                    local +
+                    "/main_01.png"
+                  }
+                  alt="메인01"
+                  title="메인01"
+                />
+              </div>
+              <div className="text-box">
+                <div>
                   <h2>
-                    LOCAL BLISS
-                    <br />
-                    TRIP
+                    {selData.tit.split("^")[0]}
+                    {selData.tit.split("^")[1]}
                   </h2>
-                  <p>
-                    국내여행에서의 특별한 추억을
-                    <br />
-                    롯데호텔과 함께 만들어보세요
-                  </p>
-                </li>
-                <li>
-                  <img
-                    src={process.env.PUBLIC_URL +"/images/branch/seoul/main_05_02.png"}
-                    alt="메인5_2"
-                  />
+                  <span className="contents-title">
+                    <p>{selData.cont.split("^")[0]}</p>
+                    <p>{selData.cont.split("^")[1]}</p>
+                    <p>{selData.cont.split("^")[2]}</p>
+                  </span>
+                </div>
+              </div>
+            </div>
+            {/* 2 */}
+            <div className="con-wrap">
+              <div className="img-box">
+                <img
+                  src={
+                    process.env.PUBLIC_URL +
+                    "/images/branch/" +
+                    local +
+                    "/main_02.png"
+                  }
+                  alt="메인02"
+                  title="메인02"
+                />
+              </div>
+              <div className="text-box">
+                <div>
                   <h2>
-                    BED & BR /EAKFAST
+                    {selData.tit02.split("^")[0]}
                     <br />
-                    PACKAGE
+                    {selData.tit02.split("^")[1]}
                   </h2>
-                  <p>
-                    고객님의 여유로운 호텔스테이를 위해
-                    <br />
-                    조식포함 상품을 준비하였습니다
-                  </p>
-                </li>
-                <li>
-                  <img
-                    src={process.env.PUBLIC_URL +"/images/branch/seoul/main_05_03.png"}
-                    alt="메인5_3"
-                  />
+                  <span className="contents-title">
+                    <p>{selData.cont02.split("^")[0]}</p>
+                    <p>{selData.cont02.split("^")[1]}</p>
+                    <p>{selData.cont02.split("^")[2]}</p>
+                  </span>
+                </div>
+              </div>
+            </div>
+            {/* 3 */}
+            <div className="con-wrap reverse">
+              <div className="img-box">
+                <img
+                  src={
+                    process.env.PUBLIC_URL +
+                    "/images/branch/" +
+                    local +
+                    "/main_03.png"
+                  }
+                  alt="메인03"
+                  title="메인03"
+                />
+              </div>
+              <div className="text-box">
+                <div>
                   <h2>
-                    LOTTE HOTEL
+                    {selData.tit03.split("^")[0]}
                     <br />
-                    REWARDS ONLY
+                    {selData.tit03.split("^")[1]}
                   </h2>
-                  <p>
-                    전 세계 롯데호텔앤리조트의 객실을
-                    <br />
-                    리워즈 회원 전용 가격으로 만나보세요
-                  </p>
-                </li>
-              </ul>
+                  <span className="contents-title">
+                    <p>{selData.cont03.split("^")[0]}</p>
+                    <p>{selData.cont03.split("^")[1]}</p>
+                    <p>{selData.cont03.split("^")[2]}</p>
+                  </span>
+                </div>
+              </div>
+            </div>
+            {/* 4 */}
+            <div className="con-wrap">
+              <h2>Room Packages & Offers</h2>
+
+              <div className="slide-box">
+                {/* 1 */}
+                <div>
+                  <div className="slide-img">
+                    <img
+                      src={
+                        process.env.PUBLIC_URL +
+                        "/images/branch/" +
+                        local +
+                        "/main_05_01.png"
+                      }
+                      alt=""
+                    />
+                  </div>
+                  <h2>{selData.tit04}</h2>
+                  <span className="contents-title">
+                    <p>{selData.cont04.split("^")[0]}</p>
+                    <p>{selData.cont04.split("^")[1]}</p>
+                  </span>
+                </div>
+                {/* 2 */}
+                <div>
+                  <div className="slide-img">
+                    <img
+                      src={
+                        process.env.PUBLIC_URL +
+                        "/images/branch/" +
+                        local +
+                        "/main_05_02.png"
+                      }
+                      alt=""
+                    />
+                  </div>
+                  <h2>{selData.tit05}</h2>
+                  <span className="contents-title">
+                    <p>{selData.cont05.split("^")[0]}</p>
+                    <p>{selData.cont05.split("^")[1]}</p>
+                  </span>
+                </div>
+                {/* 3 */}
+                <div>
+                  <div className="slide-img">
+                    <img
+                      src={
+                        process.env.PUBLIC_URL +
+                        "/images/branch/" +
+                        local +
+                        "/main_05_03.png"
+                      }
+                      alt=""
+                    />
+                  </div>
+                  <h2>{selData.tit06}</h2>
+                  <span className="contents-title">
+                    <p>{selData.cont06.split("^")[0]}</p>
+                    <p>{selData.cont06.split("^")[1]}</p>
+                  </span>
+                </div>
+              </div>
             </div>
           </div>
-        )
-      }
 
-      {/* <div className="main_06 main_0">
-                    <div className="rel">
-                        <img className="swiper" src="./img/slide_01.png" alt="스와이퍼" />
-                        <div>
-                            <h1>시그니엘 서울</h1>
-                            <p>
-                                국내 최고층 건물인 롯데월드타워 76층~101층에
-                                위치한 시그니엘 서울은 한국의 아름다움을
-                                현대적인 감각으로 풀어낸 객실(235실)에서서울의 파노라믹한 스카이라인과 환상적인 야경을
-                                조망할 수 있으며 일몰과 일출을 한 자리에서
-                                감상할 수 있는 국내 유일의 호텔입니다
-                            </p>
-                        </div>
-                        <img className="left" src="./img/left.png" alt="왼쪽" />
-                        <img className="right" src="./img/right.png" alt="오른쪽" />
-                    </div>
-                </div> */}
+          <div className="con-wrap info">
+            {local === "seoul" && <LocalMap center={latLng.current.seoul} />}
+            {local === "busan" && <LocalMap center={latLng.current.busan} />}
+            {local === "jeju" && <LocalMap center={latLng.current.jeju} />}
 
-      <div>
-        <div className="main_07">
-          <ul>
-            {local !== "seoul" && (
-              <li>
-                <a
-                  href="#"
-                  onClick={(e) => {
-                    // 기본이동막기
-                    e.preventDefault();
-                    goPage("/branch", { state: { local: "seoul" } });
-                  }}
-                >
-                  <img
-                    src={process.env.PUBLIC_URL +"/images/branch/seoul/main.jpg"}
-                    alt="호텔서울"
-                    title="호텔서울"
-                  />
-                  <p>호텔서울</p>
-                </a>
-              </li>
-            )}
-            {local !== "busan" && (
-              <li>
-                <a
-                  href="#"
-                  onClick={(e) => {
-                    // 기본이동막기
-                    e.preventDefault();
-                    goPage("/branch", { state: { local: "busan" } });
-                  }}
-                >
-                  <img
-                    src={process.env.PUBLIC_URL +"/images/branch/busan/hotel_busan.jpg"}
-                    alt="호텔부산"
-                    title="호텔부산"
-                  />
-                  <p>호텔부산</p>
-                </a>
-              </li>
-            )}
-            {local !== "jeju" && (
-              <li>
-                <a
-                  href="#"
-                  onClick={(e) => {
-                    // 기본이동막기
-                    e.preventDefault();
-                    goPage("/branch", { state: { local: "jeju" } });
-                  }}
-                >
-                  <img
-                    src={process.env.PUBLIC_URL +"/images/branch/jeju/hotel_jeju.jpg"}
-                    alt="호텔제주"
-                    title="호텔제주"
-                  />
-                  <p>호텔제주</p>
-                </a>
-              </li>
-            )}
-          </ul>
+            <ul>
+              {local !== "seoul" && (
+                <li>
+                  <a
+                    href="#"
+                    onClick={(e) => {
+                      // 기본이동막기
+                      e.preventDefault();
+                      goPage("/branch", { state: { local: "seoul" } });
+                    }}
+                  >
+                    <img
+                      src={
+                        process.env.PUBLIC_URL + "/images/branch/seoul/main.jpg"
+                      }
+                      alt="호텔서울"
+                      title="호텔서울"
+                    />
+                    <p>호텔서울</p>
+                    <p>
+                      명동, 을지로, 청계천 등 서울의 중심 관광지들로의 접근성이
+                      뛰어나 서울 관광을 위한 최적의 위치를 자랑합니다.
+                    </p>
+                  </a>
+
+                  {/* 구글맵 넣기 */}
+                  {/* <LocalMap center={latLng.current.seoul} /> */}
+                </li>
+              )}
+              {local !== "busan" && (
+                <li>
+                  <a
+                    href="#"
+                    onClick={(e) => {
+                      // 기본이동막기
+                      e.preventDefault();
+                      goPage("/branch", { state: { local: "busan" } });
+                    }}
+                  >
+                    <img
+                      src={
+                        process.env.PUBLIC_URL +
+                        "/images/branch/busan/hotel_busan.jpg"
+                      }
+                      alt="호텔부산"
+                      title="호텔부산"
+                    />
+                    <p>호텔부산</p>
+                    <p>
+                      시그니엘 부산은 해운대의 랜드마크 '엘시티(LCT)'타워에
+                      위치한 260실 규모의 럭셔리 호텔입니다.
+                    </p>
+                  </a>
+                  {/* 구글맵 넣기 */}
+                  {/* <LocalMap center={latLng.current.busan} /> */}
+                </li>
+              )}
+              {local !== "jeju" && (
+                <li>
+                  <a
+                    href="#"
+                    onClick={(e) => {
+                      // 기본이동막기
+                      e.preventDefault();
+                      goPage("/branch", { state: { local: "jeju" } });
+                    }}
+                  >
+                    <img
+                      src={
+                        process.env.PUBLIC_URL +
+                        "/images/branch/jeju/hotel_jeju.jpg"
+                      }
+                      alt="호텔제주"
+                      title="호텔제주"
+                    />
+                    <p>호텔제주</p>
+                    <p>
+                      환상의 섬 제주도 중문관광단지에 위치한 롯데호텔 제주는
+                      500개의 객실을 갖춘 한국 최고의 리조트 호텔입니다.
+                    </p>
+                  </a>
+                  {/* 구글맵 넣기 */}
+                  {/* <LocalMap center={latLng.current.jeju} /> */}
+                </li>
+              )}
+            </ul>
+          </div>
         </div>
       </div>
     </>
