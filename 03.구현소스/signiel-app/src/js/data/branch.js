b6a9ef079d8ed4ca4825ad174d56d79838a10848
ToffeeSta <Wjdgnsrh@gmail.com>
2025-03-21 14:41:08 +0900

    Revert "Merge branch 'goeun-250304'"
    This reverts commit 1b2bed027cee87ee4d56c1a798753791f1fe3a6c, reversing
    changes made to 002823b0ec001f3c3816ad2b062822ac2a790a81.

--- 03.구현소스/signiel-app/src/js/data/branch.js
+++ 03.구현소스/signiel-app/src/js/data/branch.js
@@ -1,10 +1,16 @@
 // 지점 데이터
+
+
+// 줄바꿈 하니까 에러
+// 어떻게 알아서 순서대로 들어가는지? 부산은 부산 제주는 제주
+// 글자 안에 <br> 같은 태그 어떻게 넣는지
+
 export const branchData = {
     seoul: {
         main_img:"../../../images/branch/seoul/main.jpg' alt='메인02'",
         name: "서울점",
         img01:"../../../images/branch/seoul/main_02.png' alt='메인02'",
-        tit: "Mission Make every moment of life delightful",
+        tit: "Mission<br> Make every moment of life delightful",
         cont: "인류의 매 순간을 아름답고 찬란하게 만들어 주는 것이 곳 우리의 사명입니다",
         img02:"../../../images/branch/seoul/main_03.png' alt='메인02'",
         tit02: "LOTTEHOTELS&RESORTS",
@@ -12,52 +18,31 @@ export const branchData = {
         img02:"../../../images/branch/seoul/main_04.png' alt='메인02'",
         tit03: "Vision Engrave iconic experience in the guest's heart",
         cont03: "우리만의 차별화된 경험을 선사하여 고객의 마음속에 오래 남을 브랜드가 되고자 합니다",
-
-        tit04: "LOCAL BLISS TRIP",
-        cont04: "국내여행에서의 특별한 추억을^롯데호텔과 함께 만들어보세요",
-        tit05: "BED & BR /EAKFAST^PACKAGE",
-        cont05: "고객님의 여유로운 호텔스테이를 위해^조식포함 상품을 준비하였습니다",
-        tit06: "LOTTE HOTEL^REWARDS ONLY",
-        cont06: "전 세계 롯데호텔앤리조트의 객실을^리워즈 회원 전용 가격으로 만나보세요"
     },
-    busan: {
+    pusan: {
         main_img:"../../../images/branch/busan/main.jpg' alt='메인02'",
         name: "부산점",
         img01:"../../../images/branch/busan/main_02.png' alt='메인02'",
         tit: "객실 시그니엘 부산의 객실 발코니에서 환상적인 해운대와 광안대교의 야경을 느낄 수 있습니다.",
-        cont: "- 이태리 럭셔리 베딩 브랜드 'FRETTE' 와 협업한 침구류^ - 세계적인 인테리어 디자인 기업 HBA 그룹이 바다를 테마로 디자인한 모던하고 럭셔리한 객실 ^- 차별화된 투숙객 전용 라운지 '살롱 드 시그니엘 (Salon de SIGNIEL)' : 가족고객 전용 패밀리 라운지 및 비즈니스 라운지 별도운영",
+        cont: "- 이태리 럭셔리 베딩 브랜드 'FRETTE' 와 협업한 침구류 - 세계적인 인테리어 디자인 기업 HBA 그룹이 바다를 테마로 디자인한 모던하고 럭셔리한 객실 - 차별화된 투숙객 전용 라운지 '살롱 드 시그니엘 (Salon de SIGNIEL)' : 가족고객 전용 패밀리 라운지 및 비즈니스 라운지 별도운영",
         img02:"../../../images/branch/busan/main_03.png' alt='메인02'",
         tit02: "다이닝 시그니엘 부산은 월드 클래스 셰프들이 선보이는 감각적인 미식 세계를 만끽 할 수 있는 고메 호텔 입니다.",
-        cont02: "- 더 뷰 : 8개의 쇼 키친 (Show Kitchen)에서 신선한 식재료를 사용하여 마스터 셰프 (Master Chef)들이 즉석에서 조리하는 바다 전망의 올 데이 다이닝. ^- 차오란 : 런던 미쉐린 스타 레스토랑 출신 셰프의 다채로운 요리와 독창적인 칵테일과 와인, 중국주 음료를 선보이는 모던 차이니스 타파스 바 (Modern Chinese Tapas & Bar)",
+        cont02: "- 더 뷰 : 8개의 쇼 키친 (Show Kitchen)에서 신선한 식재료를 사용하여 마스터 셰프 (Master Chef)들이 즉석에서 조리하는 바다 전망의 올 데이 다이닝. - 차오란 : 런던 미쉐린 스타 레스토랑 출신 셰프의 다채로운 요리와 독창적인 칵테일과 와인, 중국주 음료를 선보이는 모던 차이니스 타파스 바 (Modern Chinese Tapas & Bar)",
         img02:"../../../images/branch/busan/main_04.png' alt='메인02'",
-        tit03: "PT Studio, 상담실을 운영하여 과학적이고 체계적인 맞춤 프로그램을 제공합니다.",
-        cont03: "- 오션뷰의 SPA & FITNESS (스파 & 피트니스), 프라이빗 샤워부스, 다양한 온도의 탕(온탕,열탕, 냉탕), 건/습식 사우나 ^- 체성분 및 체력측정을 통한 과학적이고 체계적인 맞춤 프로그램",
-        
-        tit04: "Timeless Moments",
-        cont04: "24시간의 완벽한 여유, 짧은 일상에서 벗어나^당신의 하루를 더욱 특별하게 만들어 보세요.",
-        tit05: "SIGNIEL COLLECTION",
-        cont05: "2025년 1월 1일 시그니엘 부산 프리미엄^멤버십 '시그니엘 컬렉션'을 통해 최고의^혜택을 경험해 보세요.",
-        tit06: "Wave of retreat SIGNIEL",
-        cont06: "달콤한 초콜릿의 매력, 부드러운 초콜릿이^주는 특별한 힐링을 경험하세요.  "
+        tit03: "부대시설 이탈리아 테크노짐의 40여종 프리미엄 ARTIS LINE 장비 세팅, 유산소, 근력운동, 프리 웨이트 존 등의 다양한 목적의 운동공간을 갖추고 있습니다. PT Studio, 상담실을 운영하여 과학적이고 체계적인 맞춤 프로그램을 제공합니다.",
+        cont03: "- 오션뷰의 SPA & FITNESS (스파 & 피트니스), 프라이빗 샤워부스, 다양한 온도의 탕(온탕,열탕, 냉탕), 건/습식 사우나 - 체성분 및 체력측정을 통한 과학적이고 체계적인 맞춤 프로그램",
     },
     jeju: {
         main_img:"../../../images/branch/jeju/main.jpg' alt='메인02'",
         name: "제주점",
         img01:"../../../images/branch/busan/main_02.png' alt='메인02'",
         tit: "롯데호텔 제주는 언제나 내 집과 같은 편안함과 아늑함을 제공합니다. 아름다운 전망과 세련된 인테리어, 넓은 공간을 자랑하는 객실은 고객에게 최상의 휴식을 선사합니다.",
-        cont: "- 아름다운 야외 정원과 에메랄드빛 바다 전망 ^- 최상의 평온함을 위한 해온(he:on) 베딩 시스템 적용 (시몬스사 공동 개발)",
+        cont: "- 아름다운 야외 정원과 에메랄드빛 바다 전망 - 최상의 평온함을 위한 해온(he:on) 베딩 시스템 적용 (시몬스사 공동 개발)",
         img02:"../../../images/branch/busan/main_03.png' alt='메인02'",
         tit02: "다이닝 시그니엘 부산은 월드 클래스 셰프들이 선보이는 감각적인 미식 세계를 만끽 할 수 있는 고메 호텔 입니다.",
-        cont02: "- 라세느 : 세계 각국의 다양한 맛과 풍미를 즐길 수 있는 업스케일 뷔페 레스토랑 ^- 페닌슐라 라운지 앤 바 : 시즌별로 선보이는 달콤한 디저트와 칵테일, 위스키가 준비된 Bar & Lounge",
+        cont02: "- 라세느 : 세계 각국의 다양한 맛과 풍미를 즐길 수 있는 업스케일 뷔페 레스토랑 - 페닌슐라 라운지 앤 바 : 시즌별로 선보이는 달콤한 디저트와 칵테일, 위스키가 준비된 Bar & Lounge",
         img02:"../../../images/branch/busan/main_04.png' alt='메인02'",
         tit03: "롯데호텔 제주는 제주의 자연과 더불어 최상의 휴식을 즐길 수 있는 다양한 공간과 아이들을 위한 특별한 즐거움이 마련되어 있습니다.",
-        cont03: "- 로맨틱하고 환상적인 분위기의 사계절 럭셔리 야외 스파 & 가든 해온(he:on) ^- 가족단위 여행객을 위한 어린이 멀티 체험 놀이공간, 키즈월드",
-        
-        tit04: "25 Years Together in Jeju_Spring",
-        cont04: "롯데호텔제주 25주년 생일 맞이 봄 프로모션,^따뜻한 봄날을 롯데호텔 제주와 함께 보내는^건 어떠신가요?",
-        tit05: "Blossom Season Promotion",
-        cont05: "따스한 봄 내음 가득한날^롯데호텔 제주와 함께 봄의 향기를^만끽해 보세요.",
-        tit06: "Relaxing Getaway to V Spa",
-        cont06: "꽃 피는 봄,^롯데호텔 제주에서 즐기는 천상의 휴식"
+        cont03: "- 로맨틱하고 환상적인 분위기의 사계절 럭셔리 야외 스파 & 가든 해온(he:on) - 가족단위 여행객을 위한 어린이 멀티 체험 놀이공간, 키즈월드",
     },
 };
