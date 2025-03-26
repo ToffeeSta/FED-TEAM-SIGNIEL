bd53936aba8159c1ec9b793f83aec19c5ee02a1b
Tom B. Parker <tombap8@hotmail.com>
2025-03-26 16:22:45 +0900

    ss

--- 03.구현소스/signiel-app/src/components/pages/Branch.jsx
+++ 03.구현소스/signiel-app/src/components/pages/Branch.jsx
@@ -1,5 +1,8 @@
 import React, { useCallback, useEffect, useRef } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
 
 import { branchData } from "../../js/data/branch";
 
@@ -11,6 +14,17 @@ import LocalMap from "../modules/LocalMap";
 /* API 키값 AIzaSyCTZWygfYuMNSJHOB-p16G3P8OxI3SRUcU */
 /* 부산 35.16011, 129.1699 */
 /* 제주 33.49061, 126.4864 */
+
+// 마커 아이콘 설정 (기본 아이콘이 제대로 표시되지 않는 문제 해결)
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
+  iconUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
+  shadowUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+});
 
 function Branch(props) {
   const { state } = useLocation();
@@ -30,13 +44,94 @@ function Branch(props) {
 
   // 지역별 위도경도값
   const latLng = useRef({
-    seoul: { lat: 37.51283, lng: 127.1026 },
-    busan: { lat: 35.16011, lng: 129.1699 },
-    jeju: { lat: 33.49061, lng: 126.4864 },
+    seoul: [37.51283, 127.1026],
+    busan: [35.16011, 129.1699],
+    jeju: [33.49061, 126.4864],
   });
+
+  const EnableScrollZoomOnCtrl = () => {
+    const map = useMap();
+  
+    const handleKeyDown = (e) => {
+      if (e.key === "Control") {
+        map.scrollWheelZoom.enable();
+      }
+    };
+  
+    const handleKeyUp = (e) => {
+      if (e.key === "Control") {
+        map.scrollWheelZoom.disable();
+      }
+    };
+  
+    useEffect(() => {
+      map.scrollWheelZoom.disable(); // 기본적으로 scrollWheelZoom 비활성화
+      window.addEventListener("keydown", handleKeyDown);
+      window.addEventListener("keyup", handleKeyUp);
+  
+      return () => {
+        window.removeEventListener("keydown", handleKeyDown);
+        window.removeEventListener("keyup", handleKeyUp);
+      };
+    }, [map]);
+  
+    return null;
+  };
+    
+
+    const markerRef = useRef(null);
+
+  // 컴포넌트를 통해 줌 레벨 설정
+  const SetViewOnChange = ({ center, zoom }) => {
+    const map = useMap();
+    useEffect(() => {
+      map.setView(center, zoom);
+    }, [center, zoom, map]);
+    return null;
+  };
+
+  const OpenPopupOnLoad = ({ markerRef }) => {
+    useEffect(() => {
+      if (markerRef.current) {
+        markerRef.current.openPopup();
+      }
+    }, [markerRef]);
+    return null;
+  };
 
   // console.log(local,'지역 위도:',latLng.current[local].lat);
   // console.log(local,'지역 경도:',latLng.current[local].lng);
+
+  // 지도를 리턴하는 함수 ////////////
+  const makeMap = (local, title, zoom) => {
+    return (
+      <MapContainer
+        key={local} // key 속성 추가
+        center={latLng.current[local]}
+        zoom={zoom}
+        style={{ height: "400px", width: "100%" }}
+        scrollWheelZoom={false} // 기본적으로 scrollWheelZoom 비활성화
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <Marker position={latLng.current[local]}>
+          <Popup>
+            <div style={{ width: "200px" }}>
+              <b>{title} 지점</b>
+              <br />
+              시그니엘 호텔
+            </div>
+          </Popup>
+          <OpenPopupOnLoad markerRef={markerRef} />
+        </Marker>
+
+        <SetViewOnChange center={latLng.current[local]} zoom={zoom} />
+        <EnableScrollZoomOnCtrl />
+      </MapContainer>
+    );
+  }; ////////// makeMap 함수 /////////////
 
   // 리턴 코드구역 ///////////////
   return (
@@ -200,10 +295,20 @@ function Branch(props) {
           </div>
 
           <div className="con-wrap info">
-            {local === "seoul" && <LocalMap center={latLng.current.seoul} />}
-            {local === "busan" && <LocalMap center={latLng.current.busan} />}
-            {local === "jeju" && <LocalMap center={latLng.current.jeju} />}
-
+            {
+              // 지도리턴함수를 호출함!
+              makeMap(
+                local, // 지역코드
+                local === "seoul"
+                  ? "서울"
+                  : local === "busan"
+                  ? "부산"
+                  : local === "jeju"
+                  ? "제주"
+                  : "", // 지역 타이틀명
+                17 // 확대비율
+              )
+            }
             <ul>
               {local !== "seoul" && (
                 <li>
